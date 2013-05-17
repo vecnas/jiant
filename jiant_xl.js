a6757bbae6362424708107308123a830eea1153e
Vasily Kazantsev <vecnas@gmail.com>
2013-05-17 09:37:24 +0700

    logics returned as functions

--- jiant_xl.js
+++ jiant_xl.js
@@ -1,13 +1,23 @@
 jiant.xl = {
 
   ctl2state: function(ctl, state, selectedCssClass) {
-    ctl.click(state.go);
-    selectedCssClass && state.start(function() {
-      ctl.addClass(selectedCssClass);
-    });
-    selectedCssClass && state.end(function() {
-      ctl.removeClass(selectedCssClass);
-    });
+    return function() {
+      ctl.click(state.go);
+      selectedCssClass && state.start(function() {
+        ctl.addClass(selectedCssClass);
+      });
+      selectedCssClass && state.end(function() {
+        ctl.removeClass(selectedCssClass);
+      });
+    };
+  },
+
+  pagedContent: function(ajax, container, pager, template, renderingCb) {
+    return function() {
+      ajax(function() {
+        pager.updatePager(currentPage);
+      });
+    };
   },
 
   elementVisibilityByEvent: function(elem, eventsList) {},
