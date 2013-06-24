6c82dc67ae39f44472d638261abe6c3085eea80c
Vasily Kazantsev <vecnas@gmail.com>
2013-06-24 14:20:43 +0700

    ie xd support

--- jquery.xdomainajax.js
+++ jquery.xdomainajax.js
@@ -10,6 +10,7 @@
  * @info http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/
  */
 
+if (false)
 jQuery.ajax = (function(_ajax){
 
     var protocol = location.protocol,
@@ -53,6 +54,7 @@ jQuery.ajax = (function(_ajax){
             o.success = (function(_success){
                 return function(data) {
 
+                  jiant.logError("succ", _success, "data", data);
                     if (_success) {
                         // Fake XHR callback.
                         _success.call(this, {
@@ -73,3 +75,45 @@ jQuery.ajax = (function(_ajax){
     };
 
 })(jQuery.ajax);
+
+if (window.XDomainRequest ) {
+    jQuery.ajaxTransport(function( s ) {
+		if ( s.crossDomain && s.async ) {
+			if ( s.timeout ) {
+				s.xdrTimeout = s.timeout;
+				delete s.timeout;
+			}
+			var xdr;
+			return {
+				send: function( _, complete ) {
+					function callback( status, statusText, responses, responseHeaders ) {
+						xdr.onload = xdr.onerror = xdr.ontimeout = jQuery.noop;
+						xdr = undefined;
+						complete( status, statusText, responses, responseHeaders );
+					}
+					xdr = new XDomainRequest();
+					xdr.onload = function() {
+						callback( 200, "OK", { text: xdr.responseText }, "Content-Type: " + xdr.contentType );
+					};
+					xdr.onerror = function() {
+						callback( 404, "Not Found" );
+					};
+					xdr.onprogress = jQuery.noop;
+					xdr.ontimeout = function() {
+						callback( 0, "timeout" );
+					};
+					xdr.timeout = s.xdrTimeout || Number.MAX_VALUE;
+          s.type = "GET";
+					xdr.open( s.type, s.url );
+					xdr.send( ( s.hasContent && s.data ) || null );
+				},
+				abort: function() {
+					if ( xdr ) {
+						xdr.onerror = jQuery.noop;
+						xdr.abort();
+					}
+				}
+			};
+		}
+	});
+}
