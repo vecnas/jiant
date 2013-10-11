56d48022b9af3ba01a214f5721dd2b230fdb0685
Vasily Kazantsev <vecnas@gmail.com>
2013-10-11 15:36:22 +0700

    updated uifactory

--- jiant_stub_uifactory.js
+++ jiant_stub_uifactory.js
@@ -3,41 +3,50 @@ if (! window.jiant) {
 } else {
   jiant.setUiFactory(new function() {
 
-    function view(prefix, root, appRoot, viewId, viewContent, setupExtras) {
-      var viewElem = $("<div></div>");
+    function view(prefix, viewId) {
+      var viewElem = $("<div>" + viewId + "</div>");
       viewElem.attr("id", prefix + viewId);
       viewElem.attr("title", "view: " + viewId);
+      viewElem.attr("class", "jiant_view");
       $("body").append(viewElem);
-      $.each(viewContent, function(elemId, elemSpec) {
-        var elem;
-        if (elemSpec == jiant.input || elemSpec == jiant.inputInt) {
-          elem = $("<input type='text'/>");
-        } else if (elemSpec == jiant.ctl) {
-          elem = $("<button>" + elemId + "</button>");
-        } else {
-          alert("Unsupported element type: " + elemSpec);
-          elem = $("<div>" + elemId + "</div>");
-        }
-        elem.attr("class", prefix + elemId);
-        elem.attr("title", viewId + "." + elemId);
-        viewElem.append(elem);
-        setupExtras(appRoot, elem, elemSpec, viewId, elemId);
-      });
       return viewElem;
     }
 
-    function template(prefix, root, appRoot, tmId, tmContent) {
+    function template(prefix, tmId, tmContent) {
+      var tmElem = $("<div><div>" + tmId + "</div></div>");
+      tmElem.attr("id", prefix + tmId);
+      tmElem.attr("title", "tm: " + tmId);
+      tmElem.attr("class", "jiant_tm");
+      tmElem.hide();
+      $("body").append(tmElem);
+      return tmElem;
     }
 
-    function start() {}
+    function viewComponent(viewElem, viewId, prefix, componentId, componentContent) {
+      var elem;
+      if (componentContent == jiant.input || componentContent == jiant.inputInt) {
+        elem = $("<input type='text'/>");
+      } else if (componentContent == jiant.ctl) {
+        elem = $("<button>" + componentId + "</button>");
+      } else if (componentContent == jiant.container) {
+        elem = $("<div>" + componentId + "</div>");
+      } else if (componentContent == jiant.pager) {
+        elem = $("<div>" + componentId + "</div>");
+      } else {
+        alert("Unsupported element type: " + componentContent);
+        elem = $("<div>" + componentId + "</div>");
+      }
+      elem.attr("class", prefix + componentId + " jiant_elem");
+      elem.attr("title", viewId + "." + componentId);
+      viewElem.append(elem);
+      return elem;
+    }
 
-    function end() {}
 
     return {
-      end: end,
-      start: start,
       template: template,
-      view: view
+      view: view,
+      viewComponent: viewComponent
     }
 
   });
