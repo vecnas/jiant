a543b118539b846dae71b92dbe75e3efbc07d04b
Vasily Kazantsev <vecnas@gmail.com>
2014-09-16 11:05:38 +0700

    1.27 compatible

--- debugStates.js
+++ debugStates.js
@@ -1,25 +1,25 @@
 (function () {
 
   var logger = {
-    debugStateEndCallHandler: function(app, name, stateSpec, trace) {
+    stateEndCallHandler: function(app, name, stateSpec, trace) {
       jiant.infop("!!. STATES. called state end handler: !!, registered at ", app.id, name, trace);
     },
-    debugStateEndRegisterHandler: function(app, name, stateSpec) {
+    stateEndRegisterHandler: function(app, name, stateSpec) {
       jiant.errorp("!!. STATES. register state end handler: !!", app.id, name);
     },
-    debugStateEndTrigger: function(app, name) {
+    stateEndTrigger: function(app, name) {
       jiant.errorp("!!. STATES. trigger state end: !!", app.id, name);
     },
-    debugStateError: function(app, name, stateSpec, message) {
+    stateError: function(app, name, stateSpec, message) {
       jiant.errorp("!!. STATES. state '!!' possible error: !!", app.id, name, message);
     },
-    debugStateStartCallHandler: function(app, name, stateSpec, trace, args) {
+    stateStartCallHandler: function(app, name, stateSpec, trace, args) {
       jiant.infop("!!. STATES. called state start handler: !!, registered at ", app.id, name, trace);
     },
-    debugStateStartRegisterHandler: function(app, name, stateSpec) {
+    stateStartRegisterHandler: function(app, name, stateSpec) {
       jiant.errorp("!!. STATES. register state start handler: !!", app.id, name);
     },
-    debugStateStartTrigger: function(app, name, params) {
+    stateStartTrigger: function(app, name, params) {
       jiant.errorp("!!. STATES. trigger state start: !!, with params: !!", app.id, name, params);
     }
   };
