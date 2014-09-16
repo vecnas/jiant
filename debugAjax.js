60096337a829273330e88878581b21646f34129a
Vasily Kazantsev <vecnas@gmail.com>
2014-09-16 11:05:59 +0700

    1.27 compatible

--- debugAjax.js
+++ debugAjax.js
@@ -1,17 +1,17 @@
 (function () {
 
   var logger = {
-    debugAjaxCallStarted: function(app, uri, url, callData) {
+    ajaxCallStarted: function(app, uri, url, callData) {
       jiant.infop("!!. AJAX. call started: !!, to url !!, with params", app.id, uri, url);
       jiant.logInfo(callData);
     },
-    debugAjaxCallCompleted: function(app, uri, url, callData, timeMs) {
+    ajaxCallCompleted: function(app, uri, url, callData, timeMs) {
       jiant.infop("!!. AJAX. call completed: !!, in !! ms", app.id, uri, timeMs);
     },
-    debugAjaxCallResults: function(app, uri, url, callData, data) {
+    ajaxCallResults: function(app, uri, url, callData, data) {
       jiant.infop("!!. AJAX. call results for call: !!, results are ", app.id, uri, data);
     },
-    debugAjaxCallError: function(app, uri, url, callData, timeMs, errorMessage, jqXHR) {
+    ajaxCallError: function(app, uri, url, callData, timeMs, errorMessage, jqXHR) {
       jiant.infop("!!. AJAX. error for call: !!, time: !! ms, error is !!", app.id, uri, timeMs, errorMessage);
     }
   };
