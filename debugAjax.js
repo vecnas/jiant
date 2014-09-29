17dc86f4fb1033cad82339c4f010e218c84ad2f1
Vasily Kazantsev <vecnas@gmail.com>
2014-09-29 22:24:42 +0600

    response result printed

--- debugAjax.js
+++ debugAjax.js
@@ -9,7 +9,8 @@
       jiant.infop("!!. AJAX. call completed: !!, in !! ms", app.id, uri, timeMs);
     },
     ajaxCallResults: function(app, uri, url, callData, data) {
-      jiant.infop("!!. AJAX. call results for call: !!, results are ", app.id, uri, data);
+      jiant.infop("!!. AJAX. call results for call: !!, results are ", app.id, uri);
+      jiant.logInfo(data);
     },
     ajaxCallError: function(app, uri, url, callData, timeMs, errorMessage, jqXHR) {
       jiant.infop("!!. AJAX. error for call: !!, time: !! ms, error is !!", app.id, uri, timeMs, errorMessage);
