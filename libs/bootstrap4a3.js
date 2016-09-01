16f6d1cd200bb9af0832a38f43f971501d46165a
Vasily Kazantsev <vecnas@gmail.com>
2016-09-01 14:31:31 +0700

    Update bootstrap4a3.js

--- libs/bootstrap4a3.js
+++ libs/bootstrap4a3.js
@@ -1,1 +1,1 @@
-jiant.module("bootstrap4a3", css: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css");
+jiant.module("bootstrap4a3", {css: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css"});
