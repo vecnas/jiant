dea71af5b894072475860fcd3444b9c7881520ad
Vasily Kazantsev <vecnas@gmail.com>
2016-09-01 13:51:36 +0700

    Create bootstrap337.js

---
+++ libs/bootstrap337.js
@@ -0,0 +1,2 @@
+jiant.module("bootstrap337", {css: ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"],
+                            js: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"});
