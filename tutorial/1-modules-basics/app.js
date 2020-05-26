17d4865c74d0b6d16a71ce44a67339618edec847
vecnas <vecnas@gmail.com>
2020-05-27 01:23:01 +0700

    modules basics tutorial tuning for demo

--- tutorial/1-modules-basics/app.js
+++ tutorial/1-modules-basics/app.js
@@ -2,11 +2,16 @@
 
   jiant.DEV_MODE = true;
 
+  jiant.module("m1", ["m0"], function($, app, jiant, params, m0) {
+    $("body").append("<h1>M1 text</h1>");
+    $(document).click(() => m0.setColor("lime"));
+  });
+
   const app = {
     id: "modules-basics",
     modules: ["m0", "m1"]
   };
 
-  jiant.bindUi(app);
+  jiant.app(app);
 
 })(jiant);
--- tutorial/1-modules-basics/m1.js
+++
@@ -1,4 +0,0 @@
-jiant.module("m1", ["m0"], function($, app, jiant, params, m0) {
-  $("body").append("<h1>M1 text</h1>");
-  $(document).click(() => m0.setColor("lime"));
-});
